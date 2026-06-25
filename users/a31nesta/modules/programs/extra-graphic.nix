66f05c97364e2df12ca0533f5a6bb8b52afecfa3
A31Nesta <a31nesta@black.observer>
2026-06-25 13:52:52 +0200

    fix: update flake and noctalia keybinds in Niri

--- users/a31nesta/modules/programs/extra-graphic.nix
+++ users/a31nesta/modules/programs/extra-graphic.nix
@@ -3,7 +3,7 @@
   # Entertainment and other non-essential graphics applications
   home.packages = with pkgs; [
     vlc
-    bitwarden-desktop
+    # bitwarden-desktop # DEPENDS ON EOL ELECTRON: ISSUE: https://github.com/NixOS/nixpkgs/issues/526914
     affine
     feishin
   ];
